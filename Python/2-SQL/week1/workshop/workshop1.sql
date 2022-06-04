3ccc153b06a996a8d14c16f2d4a08bf319f53c41
Jon M Honhart <jmhonhart@gmail.com>
2022-06-04 13:01:30 -0700

    commit on main

--- Python/2-SQL/week1/workshop/workshop1.sql
+++ Python/2-SQL/week1/workshop/workshop1.sql
@@ -43,6 +43,67 @@ CREATE TABLE categories (
 
 -- TODO create more tables here...
 
+CREATE TABLE Suppliers (
+    id SERIAL,
+    name TEXT UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE customers (
+    id SERIAL,
+    company_name TEXT UNIQUE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (
+    id SERIAL,
+    first_name TEXT UNIQUE NOT NULL,
+    last_name TEXT UNIQUE NOT NULL,
+    reports_to INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE orders (
+    id SERIAL,
+    date DATE,
+    customer_id INT NOT NULL,
+    employee_id INT,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE orders_products (
+    product_id INT NOT NULL,
+    order_id INT NOT NULL,
+    quantity INT NOT NULL,
+    discount NUMERIC NOT NULL,
+    PRIMARY KEY (product_id, order_id)
+);
+
+CREATE TABLE territories (
+    id SERIAL,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees_territories (
+    employee_id INT NOT NULL, 
+    territory_id INT NOT NULL,
+    PRIMARY KEY (employee_id, territory_id)
+);
+
+CREATE TABLE offices (
+    id SERIAL,
+    address_line TEXT NOT NULL,
+    territory_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE us_states (
+    id SERIAL,
+    name TEXT NOT NULL,
+    abbreviation CHARACTER(2) NOT NULL,
+    PRIMARY KEY (id)
+);
 
 ---
 --- Add foreign key constraints
@@ -57,3 +118,49 @@ REFERENCES categories (id);
 
 
 -- TODO create more constraints here...
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_customers
+FOREIGN KEY (customer_id) 
+REFERENCES customers;
+
+ALTER TABLE orders
+ADD CONSTRAINT fk_orders_employees
+FOREIGN KEY (employee_id) 
+REFERENCES employees;
+
+ALTER TABLE products
+ADD CONSTRAINT fk_products_suppliers
+FOREIGN KEY (supplier_id) 
+REFERENCES suppliers;
+
+ALTER TABLE orders_products
+ADD CONSTRAINT fk_orders_products_orders
+FOREIGN KEY (order_id) 
+REFERENCES orders;
+
+ALTER TABLE orders_products
+ADD CONSTRAINT fk_orders_products_products
+FOREIGN KEY (product_id) 
+REFERENCES products;
+
+ALTER TABLE employees_territories
+ADD CONSTRAINT fk_employees_territories_employees
+FOREIGN KEY (employee_id) 
+REFERENCES employees;
+
+ALTER TABLE employees_territories
+ADD CONSTRAINT fk_employees_territories_territories
+FOREIGN KEY (territory_id) 
+REFERENCES territories;
+
+ALTER TABLE offices
+ADD CONSTRAINT fk_offices_territories
+FOREIGN KEY (territory_id) 
+REFERENCES territories;
+
+-- Bonus
+ALTER TABLE employees
+ADD CONSTRAINT fk_emplyees_reports_to
+FOREIGN KEY (reports_to) 
+REFERENCES employees;

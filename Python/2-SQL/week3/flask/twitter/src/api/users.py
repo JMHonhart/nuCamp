1dc5121804ceea09dddfccb10c2879e7bf364e61
Jon M Honhart <jmhonhart@gmail.com>
2022-06-18 11:13:52 -0700

    commit on main

--- Python/2-SQL/week3/flask/twitter/src/api/users.py
+++ Python/2-SQL/week3/flask/twitter/src/api/users.py
@@ -1,3 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify, abort, request
+from ..models import User, db, Tweet, likes_table
+import hashlib
+import secrets
+import sqlalchemy
 
 bp = Blueprint('users', __name__, url_prefix='/users')
+
+
+@bp.route('', methods=['GET'])  # decorator takes path and list of HTTP verbs
+def index():
+    users = User.query.all()  # ORM performs SELECT query
+    result = []
+    for u in users:
+        result.append(u.serialize())  # build list of Tweets as dictionaries
+    return jsonify(result)  # return JSON response
+
+
+@bp.route('/<int:id>', methods=['GET'])
+def show(id: int):
+    u = Tweet.query.get_or_404(id)
+    return jsonify(u.serialize())

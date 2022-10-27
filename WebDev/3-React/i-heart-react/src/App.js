1fb549b94efb6463027a20d70b2d4afb13dbf521
Jon Honhart <jmhonhart@gmail.com>
2022-10-26 19:08:42 -0700

    jonhon

--- WebDev/3-React/i-heart-react/src/App.js
+++ WebDev/3-React/i-heart-react/src/App.js
@@ -1,11 +1,27 @@
-import logo from './logo.svg';
+import HeartLogo from './heart.svg';
 import './App.css';
 
-function App(props) {
-  return(
-      <div className="App">
-        {props.msg}
-      </div>
+const message = 'cool cud';
+
+const Heart = (props) => {
+  return (
+    <div className='heart'>
+      <img className='heart-img' src={HeartLogo} alt='heart' />
+      <p className='heart-message'>{props.msg}</p>
+    </div>
+  )
+};
+
+const Header = () => {
+  return <div className='App-header'>I ❤️ React</div>
+}
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Heart msg={message} />
+    </>
   );
 }
 

6c9a678f8242f756791d865a2eacbb6b57a4cc60
Jon Honhart <jmhonhart@gmail.com>
2022-10-26 23:03:11 -0700

    Task 2: Create and render the CampsitesList compon

--- WebDev/3-React/nucampsite/src/features/campsites/CampsitesList.js
+++ WebDev/3-React/nucampsite/src/features/campsites/CampsitesList.js
@@ -7,11 +7,7 @@ const CampsitesList = () => {
     return (
         <Row className="ms-auto">{CAMPSITES.map(campsite => {
             return (
-                <Col 
-                    md='5'
-                    className="m-4"
-                    key={campsite.id}
-                >
+                <Col md='5' className="m-4" key={campsite.id}>
                     <CampsiteCard campsite={campsite} />
                 </Col>
             );

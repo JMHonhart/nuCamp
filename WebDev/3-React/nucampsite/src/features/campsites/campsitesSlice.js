531fb4ca3c7b6937cffbe0e3c2144eb6d61a3d66
Jon Honhart <jmhonhart@gmail.com>
2022-11-04 16:11:50 -0700

    CampsiteDetailPage with useParams

--- WebDev/3-React/nucampsite/src/features/campsites/campsitesSlice.js
+++ WebDev/3-React/nucampsite/src/features/campsites/campsitesSlice.js
@@ -4,8 +4,8 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectedCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === id);
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
 };
 
 export const selectFeaturedCampsite = () => {

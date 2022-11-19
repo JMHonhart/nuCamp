fee430a0831a4199a8e1b352f5cdab26d203bd0d
Jon Honhart <jmhonhart@gmail.com>
2022-11-19 09:09:32 -0800

    Week 4 Workshop Assignment Start

--- WebDev/3-React/nucampsite/Temp-Nucampsite/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { campsitesReducer } from '../features/campsites/campsitesSlice';
-
-export const store = configureStore({
-    reducer: {
-        campsites: campsitesReducer
-    }
-});
